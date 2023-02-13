1efd4dfffcd09f47e5bc1d914cb58d9cfa5e3dc0
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-13 15:14:42 +0530

    setting up our routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import store from "./utils/store";
 import "./App.css";
 import Head from "./components/Head";
 import Body from "./components/Body";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
+      <div>
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
