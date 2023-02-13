f70a603cf68822b676e0be0a490c312e8b8c7fe8
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-13 08:12:32 +0530

    creating our main container

---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import ButtonList from "./ButtonList";
+import VedioContainer from "./VideoContainer";
+
+const MainContainer = () => {
+  return (
+    <div>
+      <ButtonList />
+      <VedioContainer />
+    </div>
+  );
+};
+
+export default MainContainer;
