4db6753b53afd8499a2239b69d1b4f4e7ea67a10
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-13 09:21:16 +0530

    Providing our store to  whole app using Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Provider } from "react-redux";
+import store from "./utils/store";
 import "./App.css";
 import Head from "./components/Head";
 import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="App">
-      <Head />
-      <Body />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Head />
+        <Body />
+      </div>
+    </Provider>
   );
 }
 
