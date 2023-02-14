1fdaa3241ee81325742bd0186142cd08b11612b0
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-14 22:31:01 +0530

    Adding searchSlice to our store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import searchSlice from "./searchSlice";
 import themeSlice from "./themeSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
     theme: themeSlice,
+    search: searchSlice,
   },
 });
 
