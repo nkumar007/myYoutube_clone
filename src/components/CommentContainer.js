cb0e2360fa34a67f1ab5ded79d9fe90b71761ed3
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-15 13:54:34 +0530

    Making our comment section nested to N level

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,7 +1,110 @@
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Akshay Saini",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "Akshay Saini",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg my-2">
+      <img
+        className="w-8 h-8"
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        alt="user"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentsList = ({ comments }) => {
+  // Disclaimer: Don't use indexes as keys
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
 const CommentContainer = () => {
   return (
     <div className="m-4 p-2">
       <h1 className="text-2xl font-bold">Comments:</h1>
+      <CommentsList comments={commentsData} />
     </div>
   );
 };
